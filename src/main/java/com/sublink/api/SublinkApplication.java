fa063cd7ab87f4c89f72ca513bd7e927adf61ac3
namjincho <skawls5028@gmail.com>
2021-03-01 21:25:08 +0900

    Add User Class
    1. Genre
    2. UserAccount
    3. UserProfile
    4. UserFavorite

--- src/main/java/com/sublink/api/SublinkApplication.java
+++ src/main/java/com/sublink/api/SublinkApplication.java
@@ -1,7 +1,11 @@
 package com.sublink.api;
 
+import com.sublink.api.domain.contents.Genre;
+import com.sublink.api.domain.user.UserFavorite;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashSet;
 
 @SpringBootApplication
 public class SublinkApplication {
